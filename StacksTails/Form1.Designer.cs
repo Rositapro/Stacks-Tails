88871bc857999b30dc52b72ab402983c1df4860b
Rosalinda Cedillo <rosalindacedillo2017@gmail.com>
2024-11-30 21:35:37 -0600

    .

--- StacksTails/Form1.Designer.cs
+++ StacksTails/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(-1, 64);
+            label1.Location = new Point(-1, 60);
             label1.Name = "label1";
             label1.Size = new Size(88, 15);
             label1.TabIndex = 0;
